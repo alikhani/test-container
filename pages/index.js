28884706a741e4a5e8aa1e1d89af25bbc67823ae
alikhani <nimaalikhani5@gmail.com>
2021-03-03 12:02:44 +0100

    test

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Hello container world!
+          Hello container world!!
         </h1>
       </main>
     </div>
